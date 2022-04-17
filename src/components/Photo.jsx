f383b34c1495f4e9c324da29bdfb2d36e6e3b758
Aleksandr Shatskikh <work@errand.ru>
2022-04-17 16:13:17 +0300

    feat: done

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -3,7 +3,7 @@ export default function Photo({photo, onDelete}) {
   return (
     <div className="photoWrapper">
       <button type="submit" onClick={() => onDelete(photo.id)}>✘</button>
-      <img src={photo.url} />
+      <img src={photo.url} alt="" />
     </div>
   )
 
